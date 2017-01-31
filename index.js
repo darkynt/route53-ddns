7317471c2d87236d3f6f03c297b25dbbe31a34ec
Matt Cavanagh <matt.c@darkynt.co.uk>
2017-01-31 13:48:22 +0000

    added aws, fixed typo

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 
 const getIp = require('./lib/getip')
 const mergedConf = require('./lib/mergeconf')
+const updateIP = require('./lib/updateip')
 var lastIp
 
 console.log("Okay")
-getIp.then(ip => {
-  if (ip !== lastIp)
-  updateIP(mergedConf, ip)
-})
+const checkAndUpdate = () => {
+  getIp.then(ip => {
+    if (ip !== lastIp)
+    updateIP(mergedConf, ip)
+  })
+}
+
+setInterval(checkAndUpdate, 4000)
